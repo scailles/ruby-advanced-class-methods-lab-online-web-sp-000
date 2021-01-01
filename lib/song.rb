4b4e5547d4a733b7f3cd9f0bd1fbfd4579fa8fd9
Shaina C <scailles23@gmail.com>
2021-01-01 22:28:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,12 +49,12 @@ end
 
 
 def self.new_from_filename(filename)
-  song= self.new
-  filename.split("-")
-  song_array[1] = song_array[1].chomp(".mp3")
-  song.name = song.array[1]
-  song.artist_name = song_array[0]
-  song
+   song_array = filename.split(" - ")
+   song_array[1] = song_array[1].chomp(".mp3")
+   song = self.new
+   song.name = song_array[1]
+   song.artist_name = song_array[0]
+   song
 end
   
 end
