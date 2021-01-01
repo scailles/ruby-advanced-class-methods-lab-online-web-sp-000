eaa21f28149f0ce6a7db16d668102e67861925a3
Shaina C <scailles23@gmail.com>
2021-01-01 22:30:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -58,7 +58,7 @@ def self.new_from_filename(filename)
 end
 
 
-def create_by_filename(filename)
+def create_from_filename(filename)
     song_array = filename.split(" - ")
     song_array[1] = song_array[1].chomp(".mp3")
     song = self.new
